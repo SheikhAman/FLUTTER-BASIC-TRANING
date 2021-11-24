d301bba654ea77a2662fdd59b5fe8ab4a7459ff4
ShekAdnan <adnansk418@gmail.com>
2021-11-24 21:26:53 +0600

    setstate, stateful

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,24 +4,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green, // appbar
-            title: Text('Flutter is Fun!'),
-          ),
-          body: ListView.builder(itemBuilder: (_, index) {
-            return Container(
-              color: Colors.red,
-              height: 500,
-              width: 500,
-            );
-          })),
+        appBar: AppBar(
+          backgroundColor: Colors.green, // appbar
+          title: Text('Flutter is Fun!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                count++;
+              });
+            }),
+        body: Center(
+          child: Text('$count'),
+        ),
+      ),
     );
   }
 }
