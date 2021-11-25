84e75c7cb8c5a2567d410672b8884d9c02d349ed
ShekAdnan <adnansk418@gmail.com>
2021-11-25 17:40:17 +0600

    hero

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'about.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -17,22 +21,20 @@ class _MyAppState extends State<MyApp> {
       'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green, // appbar
-          title: Text('Flutter is Fun!'),
-        ),
-        body: IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AboutScreen()),
-            );
-          },
-          icon: Hero(tag: img, child: Image.network(img)),
-          iconSize: 50,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green, // appbar
+        title: Text('Flutter is Fun!'),
+      ),
+      body: IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AboutScreen()),
+          );
+        },
+        icon: Hero(tag: img, child: Image.network(img)),
+        iconSize: 50,
       ),
     );
   }
